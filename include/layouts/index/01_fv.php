23698346515c645338b626ee69702b8e7b7ac99d
Naoki Soga <n.soga@sunplacom>
2026-05-21 11:26:56 +0900

    ---

--- include/layouts/index/01_fv.php
+++ include/layouts/index/01_fv.php
@@ -1,3 +1,19 @@
 <div class="bg_FFFDE3 fv">
-
+    <figure class="mainFv">
+        <img src="img/mainFv.png" alt="" width="375" height="610">
+    </figure>
+    <div class="pore fvPic">
+        <figure class="poab picFvBx">
+            <img loading="lazy" src="img/picFvBx.svg" alt="" width="71.98" height="90.02">
+        </figure>
+        <figure class="txtFvCnt">
+            <img loading="lazy" src="img/txtFvCnt.svg" alt="本当に営業担当が作成する見積もりだけで安心して進められますか？" width="295.71" height="152.05">
+        </figure>
+        <p class="cl_601818 fw_500 txtset mainTxtFv">
+            未来の暮らしを想像したリフォーム計画は設備の使いやすさだけでなく、住みやすさも改善。充実した暮らしを送ることが出来ます。
+        </p>
+        <figure class="imgRehome">
+            <img loading="lazy" src="img/imgRehome.svg" alt="ハウスラボのご提案するリフォーム一級建築士だからできること" width="312" height="265.12">
+        </figure>
+    </div>
 </div>
